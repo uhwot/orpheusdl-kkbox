8de9e0ad662dda820ad72862b4a5ad85e9ccb4e9
uh wot <uhwot@protonmail.com>
2022-07-21 18:37:28 +0200

    initial commit

---
+++ interface.py
@@ -0,0 +1,328 @@
+import re
+from urllib.parse import urlparse
+from utils.models import *
+from utils.utils import create_temp_filename
+from .kkapi import KkboxAPI
+
+
+module_information = ModuleInformation(
+    service_name = 'KKBOX',
+    module_supported_modes = ModuleModes.download | ModuleModes.lyrics | ModuleModes.covers,  
+    global_settings = {'kc1_key': ''},
+    session_settings = {'email': '', 'password': ''},
+    netlocation_constant = 'kkbox', 
+    url_decoding = ManualEnum.manual,
+    login_behaviour = ManualEnum.manual,
+    test_url = 'https://play.kkbox.com/album/OspOC7CYqcVQY_uLAV'
+)
+
+
+class ModuleInterface:
+    def __init__(self, module_controller: ModuleController):
+        settings = module_controller.module_settings
+        self.exception = module_controller.module_error
+        self.tsc = module_controller.temporary_settings_controller
+        self.default_cover = module_controller.orpheus_options.default_cover_options
+        self.check_sub = not module_controller.orpheus_options.disable_subscription_check
+        if self.default_cover.file_type is ImageFileTypeEnum.webp:
+            self.default_cover.file_type = ImageFileTypeEnum.jpg
+
+        self.session = KkboxAPI(self.exception, settings['kc1_key'], settings['email'], settings['password'])
+
+        self.quality_parse = {
+            QualityEnum.MINIMUM: '128k',
+            QualityEnum.LOW: '128k',
+            QualityEnum.MEDIUM: '192k',
+            QualityEnum.HIGH: '320k',
+            QualityEnum.LOSSLESS: 'hifi',
+            QualityEnum.HIFI: 'hires'
+        }
+
+        curr_quality = self.quality_parse[module_controller.orpheus_options.quality_tier]
+        if self.check_sub and curr_quality not in self.session.available_qualities:
+            print('KKBOX: quality set in the settings is not accessible by the current subscription')
+
+    def custom_url_parse(self, link):
+        url = urlparse(link)
+
+        path_match = None
+        if url.hostname == 'play.kkbox.com':
+            path_match = re.match(r'^\/(track|album|artist|playlist)\/([a-zA-Z0-9-_]{18})', url.path)
+        elif url.hostname == 'www.kkbox.com':
+            path_match = re.match(r'^\/[a-z]{2}\/[a-z]{2}\/(song|album|artist|playlist)\/([a-zA-Z0-9-_]{18})', url.path)
+        else:
+            raise self.exception(f'Invalid URL: {link}')
+
+        if not path_match:
+            raise self.exception(f'Invalid URL: {link}')
+        
+        type = path_match.group(1)
+        if type == 'song':
+            type = 'track'
+
+        return MediaIdentification(
+            media_type = DownloadTypeEnum[type],
+            media_id = path_match.group(2)
+        )
+
+    def get_track_info(self, track_id: str, quality_tier: QualityEnum, codec_options: CodecOptions, data={}, alb_info={}) -> TrackInfo:
+        quality = self.quality_parse[quality_tier]
+        data = data.get(track_id)
+        if not data:
+            data = self.session.get_songs([track_id])[0]
+
+        tags = Tags(
+            album_artist = alb_info.get('artist_name') or data['artist_name'],
+            track_number = int(data['song_idx']),
+            total_tracks = alb_info.get('num_tracks'),
+            genres = [data['genre_name']],
+            release_date = alb_info.get('album_date')
+        )
+
+        if 'mainartist_list' in data['artist_role']:
+            data['artist_role']['mainartists'] = data['artist_role']['mainartist_list']['mainartist']
+        if 'featuredartist_list' in data['artist_role']:
+            data['artist_role']['featuredartists'] = data['artist_role']['featuredartist_list']['featuredartist']
+
+        artists = data['artist_role']['mainartists']
+        if 'featuredartists' in data['artist_role']:
+            artists.extend(data['artist_role']['featuredartists'])
+
+        error = None
+        if quality not in data['audio_quality']:
+            error = 'Track quality not available'
+        elif quality not in self.session.available_qualities:
+            error = 'Quality not available by your subscription'
+
+        codec = {
+            '128k': CodecEnum.MP3,
+            '192k': CodecEnum.MP3,
+            '320k': CodecEnum.AAC,
+            'hifi': CodecEnum.FLAC,
+            'hires': CodecEnum.FLAC,
+        }[quality]
+
+        bitrate = {
+            '128k': 128,
+            '192k': 192,
+            '320k': 320,
+            'hifi': 1411,
+            'hires': 2117,
+        }[quality]
+
+        return TrackInfo(
+            name = data.get('song_name') or data['text'],
+            album_id = data['album_more_url'].split('/')[-1] if not alb_info else alb_info['album_more_url'].split('/')[-1],
+            album = data['album_name'] if not alb_info else alb_info['album_name'],
+            artists = artists,
+            tags = tags,
+            codec = codec,
+            cover_url = self.get_img_url(data['album_photo_info']['url_template'], self.default_cover.resolution, self.default_cover.file_type),
+            release_year = int(alb_info['album_date'].split('-')[0]) if alb_info else None,
+            explicit = bool(data['song_is_explicit']),
+            artist_id = data['artist_more_url'].split('/')[-1] if not alb_info else alb_info['artist_more_url'].split('/')[-1],
+            bit_depth = 16 if quality != 'hires' else 24,
+            sample_rate = 44.1,
+            bitrate = bitrate,
+            download_extra_kwargs = {'id': data['song_more_url'].split('/')[-1], 'quality': quality},
+            cover_extra_kwargs = {'data': data},
+            lyrics_extra_kwargs = {'data': data},
+            error = error
+        )
+
+    def get_track_download(self, id, quality):
+        format = {
+            '128k': 'mp3_128k_chromecast',
+            '192k': 'mp3_192k_kkdrm1',
+            '320k': 'aac_320_download_kkdrm',
+            'hifi': 'flac_16_download_kkdrm',
+            'hires': 'flac_24_download_kkdrm',
+        }[quality]
+
+        # used for getting DRM-free mp3 128k urls
+        play_mode = None
+        if format == 'mp3_128k_chromecast':
+            play_mode = 'chromecast'
+
+        url = None
+        urls = self.session.get_ticket(id, play_mode)
+        for fmt in urls:
+            if fmt['name'] == format:
+                if format == 'mp3_128k_chromecast':
+                    return TrackDownloadInfo(
+                        download_type = DownloadEnum.URL,
+                        file_url = fmt['url'],
+                    )
+                url = fmt['url']
+                break
+
+        temp_path = create_temp_filename()
+        self.session.kkdrm_dl(url, temp_path)
+
+        return TrackDownloadInfo(
+            download_type = DownloadEnum.TEMP_FILE_PATH,
+            temp_file_path = temp_path
+        )
+
+    def get_album_info(self, album_id: str, raw_ids={}) -> Optional[AlbumInfo]:
+        raw_id = raw_ids.get(album_id)
+        if not raw_id:
+            raw_id = self.session.get_album(album_id)['album']['album_id']
+
+        data = self.session.get_album_more(raw_id)
+
+        info = data['info']
+
+        data_kwargs = {}
+        song_id_list = []
+        for song in data['song_list']['song']:
+            id = song['song_more_url'].split('/')[-1]
+            song_id_list.append(id)
+            data_kwargs[id] = song
+        
+        info['num_tracks'] = len(song_id_list)
+
+        return AlbumInfo(
+            name = info['album_name'],
+            artist = info['artist_name'],
+            tracks = song_id_list,
+            release_year = int(info['album_date'].split('-')[0]),
+            explicit = bool(info['album_is_explicit']),
+            artist_id = info['artist_more_url'].split('/')[-1],
+            cover_url = self.get_img_url(info['album_photo_info']['url_template'], self.default_cover.resolution, self.default_cover.file_type),
+            cover_type = self.default_cover.file_type,
+            all_track_cover_jpg_url = self.get_img_url(info['album_photo_info']['url_template'], self.default_cover.resolution, ImageFileTypeEnum.jpg),
+            description = info['album_descr'],
+            track_extra_kwargs = {'data': data_kwargs, 'alb_info': info}
+        )
+
+    def get_playlist_info(self, playlist_id: str) -> PlaylistInfo:
+        data = self.session.get_playlists([playlist_id])[0]
+
+        data_kwargs = {}
+        song_id_list = []
+        for song in data['songs']:
+            id = song['song_more_url'].split('/')[-1]
+            song_id_list.append(id)
+            data_kwargs[id] = song
+
+        return PlaylistInfo(
+            name = data['title'],
+            creator = data['user']['name'] if data['user'] else None,
+            tracks = song_id_list,
+            release_year = int(data['created_at'].split('-')[0]),
+            creator_id = data['user']['id'] if data['user'] else None,
+            cover_url = self.get_img_url(data['cover_photo_info']['url_template'], self.default_cover.resolution, self.default_cover.file_type),
+            cover_type = self.default_cover.file_type,
+            description = data['content'],
+            track_extra_kwargs = {'data': data_kwargs}
+        )
+
+    def get_artist_info(self, artist_id: str, get_credited_albums: bool, data=None) -> ArtistInfo:
+        profile = data
+        albums = []
+        offset = 0
+        if not data:
+            data = self.session.get_artist(artist_id)
+            profile = data['profile']
+            albums = data['album']
+            offset = 10
+
+        if len(albums) == 10:
+            albums.extend(self.session.get_artist_albums(profile['artist_id'], 8008135, offset))
+
+        raw_ids = {}
+        album_id_list = []
+        for album in albums:
+            id = album['encrypted_album_id']
+            album_id_list.append(id)
+            raw_ids[id] = album['album_id']
+
+        return ArtistInfo(
+            name = profile['artist_name'],
+            albums = album_id_list,
+            album_extra_kwargs = {'raw_ids': raw_ids},
+        )
+
+    def get_track_cover(self, track_id: str, cover_options: CoverOptions, data=None) -> CoverInfo:
+        data = data or self.session.get_songs([track_id])[0]
+        url_template = data['album_photo_info']['url_template']
+        url = self.get_img_url(url_template, cover_options.resolution, cover_options.file_type)
+        return CoverInfo(url=url, file_type=cover_options.file_type)
+
+    def get_track_lyrics(self, track_id: str, data={}) -> LyricsInfo:
+        if data.get('is_lyrics') == False or data.get('song_lyrics_valid') == 0:
+            return LyricsInfo()
+        resp = self.session.get_song_lyrics(track_id)
+        if resp['status']['type'] != 'OK':
+            return LyricsInfo()
+
+        embedded = ''
+        synced = ''
+        for lyr in resp['data']['lyrics']:
+            if not lyr['content']:
+                embedded += '\n'
+                synced += '\n'
+                continue
+
+            time = lyr['start_time']
+            min = int(time / (1000 * 60))
+            sec = int(time / 1000) % 60
+            ms = int(time % 100)
+            time_tag = f'[{min:02d}:{sec:02d}.{ms:02d}]'
+
+            embedded += lyr['content'] + '\n'
+            synced += time_tag + lyr['content'] + '\n'
+
+        return LyricsInfo(embedded, synced)
+
+    def search(self, query_type: DownloadTypeEnum, query: str, track_info: TrackInfo = None, limit: int = 10):
+        query_type = query_type.name
+        if query_type == 'track':
+            query_type = 'song'
+
+        # tfw this shitty streaming service has no way to search for ISRCs
+
+        results = self.session.search(query, [query_type], limit)[f'{query_type}_list'][query_type]
+
+        if query_type == 'song':
+            search_results = []
+            for i in results:
+                artists = i['artist_role']['mainartist_list']['mainartist']
+                if 'featuredartist_list' in i['artist_role']:
+                    artists.extend(i['artist_role']['featuredartist_list']['featuredartist'])
+            
+                search_results.append(SearchResult(
+                    result_id = i['song_more_url'].split('/')[-1],
+                    name = i['song_name'],
+                    artists = artists,
+                    explicit = i['song_is_explicit'],
+                    additional = [i['album_name']],
+                    extra_kwargs = {'data': i}
+                ))
+            return search_results
+        elif query_type == 'album':
+            return [SearchResult(
+                result_id = i['album_more_url'].split('/')[-1],
+                name = i['album_name'],
+                artists = [i['artist_name']],
+                explicit = i['album_is_explicit'],
+                extra_kwargs = {'raw_ids': {i['album_more_url'].split('/')[-1]: i['album_id']}}
+            ) for i in results]
+        elif query_type == 'artist':
+            return [SearchResult(
+                result_id = i['artist_more_url'].split('/')[-1],
+                name = i['artist_name'],
+                extra_kwargs = {'data': i}
+            ) for i in results]
+        elif query_type == 'playlist':
+            return [SearchResult(
+                result_id = i['id'],
+                name = i['title'],
+                artists = [i['user']['name']],
+                additional = [i['content']]
+            ) for i in results]
+
+    def get_img_url(self, url_template, size, file_type: ImageFileTypeEnum):
+        # not using .format() here because of possible data leak vulnerabilities
+        return url_template.replace('{width}', str(size)).replace('{height}', str(size)).replace('{format}', file_type.name)
---
+++ kkapi.py
@@ -0,0 +1,218 @@
+import json
+from time import time, sleep
+from random import randrange
+from Cryptodome.Cipher import ARC4
+from Cryptodome.Hash import MD5
+from tqdm import tqdm
+from utils.utils import create_requests_session
+
+class KkboxAPI:
+    def __init__(self, exception, kc1_key, email, password):
+        self.exception = exception
+        self.kc1_key = kc1_key.encode('ascii')
+
+        self.s = create_requests_session()
+        self.s.headers.update({
+            'user-agent': 'okhttp/3.14.9'
+        })
+
+        self.kkid = '%030X' % randrange(16**32)
+
+        self.hosts = {
+            'ds': 'api-ds-http2.kkbox.com.tw',
+            'login': 'api-login-http2.kkbox.com.tw',
+            'ticket': 'api-ticket.kkbox.com.tw',
+        }
+
+        self.params = {
+            'enc': 'u',
+            'ver': '06090076',
+            'os': 'android',
+            'osver': '11',
+            'lang': 'en',
+            'ui_lang': 'en',
+            'dist': '0021',
+            'dist2': '0021',
+            'resolution': '411x683',
+            'of': 'j',
+            'oenc': 'kc1',
+        }
+
+        self.login(email, password)
+
+    def kc1_decrypt(self, data):
+        cipher = ARC4.new(self.kc1_key)
+        return cipher.decrypt(data).decode('utf-8')
+
+    def api_call(self, host, path, params={}, payload=None):
+        if host == 'ticket':
+            payload = json.dumps(payload)
+
+        host = self.hosts[host]
+        params.update(self.params)
+        params.update({'timestamp': int(time())})
+
+        url = f'https://{host}/{path}'
+        if not payload:
+            r = self.s.get(url, params=params)
+        else:
+            r = self.s.post(url, params=params, data=payload)
+
+        resp = json.loads(self.kc1_decrypt(r.content))
+        return resp
+
+    def login(self, email, password):
+        md5 = MD5.new()
+        md5.update(password.encode('utf-8'))
+        password = md5.hexdigest()
+
+        resp = self.api_call('login', 'login.php', payload={
+            'uid': email,
+            'passwd': password,
+            'kkid': self.kkid,
+            'registration_id': '',
+        })
+
+        if resp['status'] != -4:
+            if resp['status'] == -1:
+                raise self.exception('Email not found')
+            elif resp['status'] == -2:
+                raise self.exception('Incorrect password')
+            raise self.exception('Login failed')
+        
+        self.apply_session(resp)
+
+    def renew_session(self):
+        resp = self.api_call('login', 'check.php')
+        if resp['status'] != -4:
+            raise self.exception('Session renewal failed')
+        self.apply_session(resp)
+
+    def apply_session(self, resp):
+        self.sid = resp['sid']
+        self.params['sid'] = self.sid
+
+        self.lic_content_key = resp['lic_content_key'].encode('ascii')
+
+        self.available_qualities = ['128k', '192k', '320k']
+        if resp['high_quality']:
+            self.available_qualities.append('hifi')
+            self.available_qualities.append('hires')
+
+    def get_songs(self, ids):
+        resp = self.api_call('ds', 'v2/song', payload={
+            'ids': ','.join(ids),
+            'fields': 'artist_role,song_idx,album_photo_info,song_is_explicit,song_more_url,album_more_url,artist_more_url,genre_name,is_lyrics,audio_quality'
+        })
+        if resp['status']['type'] != 'OK':
+            raise self.exception('Track not found')
+        return resp['data']['songs']
+
+    def get_song_lyrics(self, id):
+        return self.api_call('ds', f'v1/song/{id}/lyrics')
+
+    def get_album(self, id):
+        resp = self.api_call('ds', f'v1/album/{id}')
+        if resp['status']['type'] != 'OK':
+            raise self.exception('Album not found')
+        return resp['data']
+
+    def get_album_more(self, raw_id):
+        return self.api_call('ds', 'album_more.php', params={
+            'album': raw_id
+        })
+
+    def get_artist(self, id):
+        resp = self.api_call('ds', f'v3/artist/{id}')
+        if resp['status']['type'] != 'OK':
+            raise self.exception('Artist not found')
+        return resp['data']
+    
+    def get_artist_albums(self, raw_id, limit, offset):
+        resp = self.api_call('ds', f'v2/artist/{raw_id}/album', params={
+            'limit': limit,
+            'offset': offset,
+        })
+        if resp['status']['type'] != 'OK':
+            raise self.exception('Artist not found')
+        return resp['data']['album']
+
+    def get_playlists(self, ids):
+        resp = self.api_call('ds', f'v1/playlists', params={
+            'playlist_ids': ','.join(ids)
+        })
+        if resp['status']['type'] != 'OK':
+            raise self.exception('Playlist not found')
+        return resp['data']['playlists']
+
+    def search(self, query, types, limit):
+        return self.api_call('ds', 'search_music.php', params={
+            'sf': ','.join(types),
+            'limit': limit,
+            'query': query,
+            'search_ranking': 'sc-A',
+        })
+
+    def get_ticket(self, song_id, play_mode = None):
+        resp = self.api_call('ticket', 'v1/ticket', payload={
+            'sid': self.sid,
+            'song_id': song_id,
+            'ver': '06090076',
+            'os': 'android',
+            'osver': '11',
+            'kkid': self.kkid,
+            'dist': '0021',
+            'dist2': '0021',
+            'timestamp': int(time()),
+            'play_mode': play_mode,
+        })
+
+        if resp['status'] != 1:
+            if resp['status'] == -1:
+                self.renew_session()
+                return self.get_ticket(song_id, play_mode)
+            elif resp['status'] == -4:
+                self.auth_device()
+                return self.get_ticket(song_id, play_mode)
+            elif resp['status'] == 2:
+                # tbh i'm not sure if this is some rate-limiting thing
+                # or if it's a bug on their slow-as-hell servers
+                sleep(1)
+                return self.get_ticket(song_id, play_mode)
+            raise self.exception("Couldn't get track URLs")
+
+        return resp['uris']
+
+    def auth_device(self):
+        resp = self.api_call('ds', 'active_sid.php', payload={
+            'ui_lang': 'en',
+            'of': 'j',
+            'os': 'android',
+            'enc': 'u',
+            'sid': self.sid,
+            'ver': '06090076',
+            'kkid': self.kkid,
+            'lang': 'en',
+            'oenc': 'kc1',
+            'osver': '11',
+        })
+        if resp['status'] != 1:
+            raise self.exception("Couldn't auth device")
+
+    def kkdrm_dl(self, url, path):
+        # skip first 1024 bytes of track file
+        resp = self.s.get(url, stream=True, headers={'range': 'bytes=1024-'})
+        resp.raise_for_status()
+
+        size = int(resp.headers['content-length'])
+        bar = tqdm(total=size, unit='B', unit_scale=True)
+
+        # drop 512 bytes of keystream
+        rc4 = ARC4.new(self.lic_content_key, drop=512)
+
+        with open(path, 'wb') as f:
+            for chunk in resp.iter_content(chunk_size=4096):
+                f.write(rc4.decrypt(chunk))
+                bar.update(len(chunk))
+
+        bar.close()
